8bd8ef57f43902fc90b68b5b49527015b9288739
Arkar Aung <arica.aung@gmail.com>
2015-02-19 20:23:37 +0630

    Parse String, Number and Atom

--- simpleparser.hs
+++ simpleparser.hs
@@ -1,7 +1,16 @@
 module Main where
+
+import Control.Monad
 import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
 
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+               
 main :: IO ()
 main = do args <- getArgs
           putStrLn (readExpr (args !! 0))
@@ -13,6 +22,29 @@ spaces :: Parser ()
 spaces = skipMany1 space
 
 readExpr :: String -> String
-readExpr input = case parse (spaces >> symbol)  "lisp" input of
+readExpr input = case parse parseExpr "lisp" input of
   Left err -> "No match: " ++ show err
   Right val -> "Found value"
+
+parseString :: Parser LispVal
+parseString = do char '"'
+                 x <- many (noneOf "\"")
+                 char '"'
+                 return $ String x
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseAtom :: Parser LispVal
+parseAtom = do first <- letter <|> symbol
+               rest <- many (letter <|> digit <|> symbol)
+               let atom = [first] ++ rest
+               return $ case atom of
+                         "#t" -> Bool True
+                         "#f" -> Bool False
+                         otherwise -> Atom atom
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+            <|> parseString
+            <|> parseNumber
